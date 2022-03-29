4a94071f4e018a9f35bb5c6dab45ba15e7b06f22
Dan Tovbein <dtovbein@gmail.com>
2022-03-28 21:41:30 -0300

    Update services

--- src/services/greenlab.ts
+++ src/services/greenlab.ts
@@ -16,6 +16,22 @@ export const createContact = async (data: any) => {
   }
 }
 
+export const updateContact = async (email: string, data: any) => {
+  try {
+    const response = await ApiCall({
+      headers: {
+        'X-Greenlab-App': window.sessionStorage.getItem('greenlab_app'),
+      },
+      baseURL: `${process.env.REACT_APP_GREENLAB_API_URL}/hubspot/contact/email/${email}`,
+      method: 'POST',
+      data,
+    });
+    return response;
+  } catch(error: any) {
+    console.log(error);
+  }
+}
+
 export const getCoupon = async (appName = '') => {
   try {
     const response = await ApiCall({
@@ -24,6 +40,22 @@ export const getCoupon = async (appName = '') => {
       },
       baseURL: `${process.env.REACT_APP_GREENLAB_API_URL}/application/coupon/${appName}?env=${process.env.REACT_APP_ENVIRONMENT}`,
       method: 'GET',
+    });
+    return response;
+  } catch(error: any) {
+    console.log(error);
+  }
+}
+
+export const postRecord = async (data: any) => {
+  try {
+    const response = await ApiCall({
+      headers: {
+        'X-Greenlab-App': window.sessionStorage.getItem('greenlab_app'),
+      },
+      baseURL: `${process.env.REACT_APP_GREENLAB_API_URL}/forma/form/${data.form_id}/record`,
+      method: 'POST',
+      data,
     });
     return response;
   } catch(error: any) {

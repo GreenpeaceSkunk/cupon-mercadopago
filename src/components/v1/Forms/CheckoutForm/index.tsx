c8a1736978507ce44b48c153f61eb6bec9856230
Dan Tovbein <dtovbein@gmail.com>
2022-05-04 14:09:30 -0300

    Create v1 of components

---
+++ src/components/v1/Forms/CheckoutForm/index.tsx
@@ -0,0 +1,455 @@
+import React, { FormEvent, memo, useCallback, useContext, useState, useRef, useReducer, useMemo, useEffect } from 'react';
+import { generatePath, useNavigate } from 'react-router-dom';
+import { FormContext } from '../../../Forms/context';
+import { OnChangeEvent } from 'greenpeace';
+import { validateCardHolderName, validateCitizenId, validateCreditCard, validateCvv, validateEmptyField, validateMonth, validateYear } from '../../../../utils/validators';
+import { css } from 'styled-components';
+import Elements from '../../../Shared/Elements';
+import { getPublicKey, doSubscriptionPayment } from '../../../../services/mercadopago';
+import Shared from '../../../Shared';
+import Form from '../../Shared/Form';
+import { initialState, reducer } from '../../../Forms/CheckoutForm/reducer';
+import { createToken, getCardType, getInstallments, setPublishableKey } from '../../../../utils/mercadopago';
+import useQuery from '../../../../hooks/useQuery';
+import Snackbar, { IRef as ISnackbarRef } from '../../../Snackbar';
+import { AppContext } from '../../../App/context';
+import { postRecord, updateContact } from '../../../../services/greenlab';
+
+const Component: React.FunctionComponent<{}> = memo(() => {
+  const { appData } = useContext(AppContext);
+  const { data: { payment, user }, params, dispatch } = useContext(FormContext);
+  const [{ submitting, submitted, allowNext, error, attemps }, dispatchFormErrors ] = useReducer(reducer, initialState);
+  const [ showFieldErrors, setShowFieldErrors ] = useState<boolean>(false);
+  const navigate = useNavigate();
+  const formRef = useRef<HTMLFormElement>(null);
+  const snackbarRef = useRef<ISnackbarRef>(null);
+  const { searchParams, urlSearchParams } = useQuery();
+
+  const onChangeHandler = useCallback((evt: OnChangeEvent) => {
+    evt.preventDefault();
+    dispatch({
+      type: 'UPDATE_PAYMENT_DATA',
+      payload: { [ evt.currentTarget.name ]: evt.currentTarget.value }
+    });
+  }, [
+    dispatch,
+  ]);
+
+  const onUpdateFieldHandler = useCallback((fieldName: string, isValid: boolean, value: any) => {
+    dispatchFormErrors({
+      type: 'UPDATE_FIELD_ERRORS',
+      payload: {
+        fieldName,
+        isValid,
+      }
+    });
+  }, []);
+
+  /**
+   * Backup to Forma.
+   * @param payload What is needed to be "backuped"
+   */
+  const backupInformation = useCallback(( payload = null ) => {
+    (async () => {
+      console.log('Backup information', payload);
+
+      await updateContact(payload.email, {
+        donationStatus: payload.donationStatus,
+      });
+      
+      if(appData && appData.settings && appData.settings.service) {
+        const { service } = appData.settings;
+  
+        if(service.forma.transactions_form) {
+          await postRecord({
+            amount: payload.amount,
+            areaCode: payload.cod_area,
+            campaignId: payload.campaign_id,
+            card: payload.card,
+            card_type: payload.card_type,
+            cardLastDigits: payload.lastDigits,
+            cardExpMonth: payload.mes_vencimiento,
+            cardExpYear: payload.ano_vencimiento,
+            citizenId: payload.documento,
+            citizenIdType: payload.tipodocumento,
+            mpDeviceId: payload.device_id,
+            donationStatus: payload.donationStatus,
+            email: payload.email,
+            errorCode: payload.errorCode || '',
+            errorMessage: payload.errorMessage || '',
+            firstName: payload.nombre,
+            form_id: service.forma.transactions_form,
+            fromUrl: document.location.href,
+            lastName: payload.apellido,
+            mpPayMethodId: payload.payment_method_id,
+            mpPayOptId: payload.payment_method_option_id,
+            phoneNumber: payload.telefono,
+            recurrenceDay: payload.tomorrow,
+            transactionDate: payload.date,
+            userAgent: window.navigator.userAgent.replace(/;/g, '').replace(/,/g, ''),
+            utm: `utm_campaign=${ urlSearchParams.get('utm_campaign')}&utm_medium=${ urlSearchParams.get('utm_medium')}&utm_source=${ urlSearchParams.get('utm_source')}&utm_content=${ urlSearchParams.get('utm_content')}&utm_term=${ urlSearchParams.get('utm_term')}`,
+          });
+        }
+      }
+
+      const timer = setTimeout(() => {
+        dispatchFormErrors({ type: 'SUBMITTED' });
+        navigate({
+          pathname: generatePath(`/:couponType/forms/thankyou`, {
+            couponType: params.couponType,
+          }),
+          search: `${searchParams}`,
+        }, { replace: true });
+      }, 250);
+
+      return () => {
+        clearTimeout(timer);
+      }
+    })();
+  }, [
+    dispatchFormErrors,
+  ]);
+
+  const onSubmitHandler = useCallback(async (evt: FormEvent) => {
+    evt.preventDefault();
+    
+    if(!allowNext) {
+      setShowFieldErrors(true);
+      // dispatchFormErrors({
+      //   type: 'SET_ERROR',
+      //   error: 'Tenés campos incompletos o con errores. Revisalos para continuar.',
+      // });
+    } else {
+      (async () => {
+        dispatchFormErrors({ type: 'SUBMIT' });
+  
+        if(formRef.current) {
+          setPublishableKey(await getPublicKey());
+          const token = await createToken(formRef.current);
+          const amount = payment.amount === 'otherAmount' ? payment.newAmount : payment.amount;
+          
+          if(token.isValid) {
+            const paymentMethod = await getInstallments({
+              bin: payment.cardNumber.slice(0, 6),
+              amount,
+            });
+
+            if(paymentMethod) {
+              const merchantAccounts = (paymentMethod.agreements.length) ? paymentMethod.agreements[0].merchant_accounts : [];
+              let merchantAccount = merchantAccounts.filter((a: any) => {
+                if(`${process.env.REACT_APP_COUPON_TYPE}` === 'regular' && paymentMethod.payment_method_id === 'amex') {
+                  return a;
+                }
+                if (params.couponType === 'regular' && a.branch_id === 'regular') {
+                  return a;
+                }
+                if(params.couponType === 'oneoff' && a.branch_id === null) {
+                  return a;
+                }
+                return a;
+              });
+
+              const today = new Date();
+              const tomorrow = new Date(today);
+              tomorrow.setDate(tomorrow.getDate() + 1);
+              
+              let payload = {
+                device_id: window.MP_DEVICE_SESSION_ID,
+                payment_method_id: paymentMethod.payment_method_id,
+                issuer_id: paymentMethod.issuer.id,
+                token: window.Mercadopago.tokenId,
+                type: params.couponType,
+                merchant_account_id: (merchantAccount.length) ? merchantAccount[0].id : null,
+                payment_method_option_id: (merchantAccount.length) ? merchantAccount[0].payment_method_option_id : null,
+                amount,
+                nombre: user.firstName,
+                apellido: user.lastName,
+                cod_area: user.areaCode,
+                telefono: user.phoneNumber,
+                email: user.email,
+                genero: '',
+                pais: '',
+                direccion: '',
+                localidad: '',
+                provincia: '',
+                codigo_provincia: '',
+                codigo_postal: '',
+                ocupacion: '',
+                tipodocumento: payment.docType,
+                mes_vencimiento: payment.cardExpirationMonth,
+                ano_vencimiento: payment.cardExpirationYear,
+                documento: payment.docNumber,
+                firstDigits: payment.cardNumber.slice(0, 6),
+                lastDigits: payment.cardNumber.slice(payment.cardNumber.length - 4),
+                date: today,
+                today: today.getDate(),
+                tomorrow: tomorrow.getDate(),
+                utms: [
+                  { campo: 'gpi__utm_campaign__c', valor: urlSearchParams.get('utm_campaign') },
+                  { campo: 'gpi__utm_medium__c', valor: urlSearchParams.get('utm_medium') },
+                  { campo: 'gpi__utm_source__c', valor: urlSearchParams.get('utm_source') },
+                  { campo: 'gpi__utm_content__c', valor: urlSearchParams.get('utm_content') },
+                  { campo: 'gpi__utm_term__c', valor: urlSearchParams.get('utm_term') },
+                ],
+                campaign_id: `${appData.settings.tracking.salesforce.campaign_id}`,
+              };
+              
+              const result = await doSubscriptionPayment(payload);
+
+              payload = {...payload, ...{
+                card: payment.cardNumber,
+                card_type: getCardType(paymentMethod.payment_method_id),
+                payment_method_id: paymentMethod.issuer.name,
+              }};
+              
+              if(result['error']) {
+                backupInformation({...payload, donationStatus: 'pending', errorCode: result.errorCode, errorMessage: result.message.replace(/,/g, '').replace(/;/g, '') });
+              } else {
+                window.userAmount = amount;
+                backupInformation({...payload, donationStatus: 'done'});
+                
+                return () => {
+                  paymentMethod.cancel();
+                  result.cancel();
+                }
+              }
+            } else {
+              console.log('Ocurrió un error inesperado, pruebe con otra tarjeta.');
+              dispatchFormErrors({
+                type: 'SUBMITTED_WITH_ERRORS',
+                error: 'Ocurrió un error inesperado, pruebe con otra tarjeta.',
+              });
+            }
+          } else {
+            console.log('No se creó el Token %s', token.message);
+            dispatchFormErrors({
+              type: 'SUBMITTED_WITH_ERRORS',
+              error: token.message,
+            });
+          }
+        }
+      })();
+    }
+  }, [
+    allowNext,
+    searchParams,
+    formRef,
+    payment,
+    user,
+    navigate,
+    params,
+    urlSearchParams,
+    appData,
+  ]);
+  
+  useEffect(() => {
+    if(error) {
+      if(snackbarRef && snackbarRef.current) {
+        snackbarRef.current.showSnackbar();
+      }
+    }
+  }, [
+    error,
+  ]);
+
+  return useMemo(() => (
+    <Form.Main
+      id='transaction-form'
+      ref={formRef}
+      onSubmit={onSubmitHandler}
+    >
+      <Form.Header>
+        <Elements.HGroup>
+          <Form.Title>{appData && appData.content && appData.content.form.checkout.title}</Form.Title>
+        </Elements.HGroup>
+        <Shared.General.Text>{appData && appData.content && appData.content.form.checkout.text}</Shared.General.Text>
+      </Form.Header>
+      <Form.Content>
+        <Form.Row>
+          <Form.Column bottomText='Escribe solo números. No agregues guiones.'>
+            <Form.Group
+              fieldName='cardNumber'
+              value={payment.cardNumber}
+              labelText='Número de la tarjeta'
+              showErrorMessage={showFieldErrors}
+              validateFn={validateCreditCard}
+              onUpdateHandler={onUpdateFieldHandler}
+              >
+              <Elements.Input
+                type='text'
+                id='cardNumber'
+                name='cardNumber'
+                placeholder='Ej. 4509953566233704'
+                data-checkout='cardNumber'
+                maxLength={16}
+                value={payment.cardNumber}
+                onChange={onChangeHandler}
+                />
+            </Form.Group>
+          </Form.Column>
+        </Form.Row>
+        <Form.Row>
+          <Form.Column>
+            <Form.Group
+              fieldName='securityCode'
+              value={payment.securityCode}
+              labelText='Código de seguridad'
+              showErrorMessage={showFieldErrors}
+              validateFn={validateCvv}
+              onUpdateHandler={onUpdateFieldHandler}
+            >
+              <Elements.Input
+                type='password'
+                id='securityCode'
+                name='securityCode'
+                placeholder='Ej. 123'
+                data-checkout='securityCode'
+                maxLength={4}
+                value={payment.securityCode}
+                onChange={onChangeHandler}
+              />
+            </Form.Group>
+          </Form.Column>
+        </Form.Row>
+        <Form.Row>
+          <Form.Column>
+            <Form.Group
+              fieldName='cardExpirationMonth'
+              value={payment.cardExpirationMonth}
+              labelText='Mes de expiración'
+              showErrorMessage={showFieldErrors}
+              validateFn={validateMonth}
+              onUpdateHandler={onUpdateFieldHandler}
+            >
+              <Elements.Select
+                id='cardExpirationMonth'
+                name='cardExpirationMonth'
+                data-checkout='cardExpirationMonth'
+                value={payment.cardExpirationMonth}
+                onChange={onChangeHandler}
+              >
+                <option></option>
+                {(['01','02','03','04','05','06','07','08','09','10','11','12']).map((value: string, key: number) => (
+                  <option key={key} value={value}>{value}</option>
+                ))}
+              </Elements.Select>
+            </Form.Group>
+            <Form.Group
+              fieldName='cardExpirationYear'
+              value={payment.cardExpirationYear}
+              labelText='Año de expiración'
+              showErrorMessage={showFieldErrors}
+              validateFn={validateYear}
+              onUpdateHandler={onUpdateFieldHandler}
+            >
+              <Elements.Select
+                id='cardExpirationYear'
+                name='cardExpirationYear'
+                data-checkout='cardExpirationYear'
+                value={payment.cardExpirationYear}
+                onChange={onChangeHandler}
+              >
+                {(Array.from(Array(20).keys()).map((value) => value + (new Date().getFullYear()))).map((value: number, key: number) => (
+                  <option key={key} value={value}>{value}</option>
+                ))}
+              </Elements.Select>
+            </Form.Group>
+          </Form.Column>
+        </Form.Row>
+        <Form.Row>
+          <Form.Column>
+            <Form.Group
+              fieldName='docType'
+              value={payment.docType}
+              labelText='Tipo de documento'
+              showErrorMessage={showFieldErrors}
+              validateFn={validateEmptyField}
+              onUpdateHandler={onUpdateFieldHandler}
+            >
+              <Elements.Select
+                id='docType'
+                name='docType'
+                data-checkout='docType'
+                value={payment.docType}
+                onChange={onChangeHandler}
+              >
+                <option></option>
+                {(['DNI', 'Cédula de identidad', 'LC', 'LE', 'Otro']).map((value: string, key: number) => (
+                  <option key={key} value={value}>{value}</option>
+                ))}
+              </Elements.Select>
+            </Form.Group>
+            <Form.Group
+              fieldName='docNumber'
+              value={payment.docNumber}
+              labelText='Número de documento'
+              showErrorMessage={showFieldErrors}
+              validateFn={validateCitizenId}
+              onUpdateHandler={onUpdateFieldHandler}
+            >
+              <Elements.Input
+                type='text'
+                id='docNumber'
+                name='docNumber'
+                placeholder='Ej. 31402931'
+                data-checkout='docNumber'
+                maxLength={8}
+                value={payment.docNumber}
+                onChange={onChangeHandler}
+              />
+            </Form.Group>
+          </Form.Column>
+        </Form.Row>
+        <Form.Row>
+          <Form.Column>
+            <Form.Group
+              value={payment.cardholderName}
+              fieldName='cardholderName'
+              labelText='Titular de la tarjeta'
+              validateFn={validateCardHolderName}
+              onUpdateHandler={onUpdateFieldHandler}
+              showErrorMessage={showFieldErrors}
+            >
+              <Elements.Input
+                type='text'
+                id='cardholderName'
+                name='cardholderName'
+                data-checkout='cardholderName'
+                placeholder='Ej. Daniela Lopez'
+                value={payment.cardholderName}
+                onChange={onChangeHandler}
+              />
+            </Form.Group>
+          </Form.Column>
+        </Form.Row>
+      </Form.Content>
+      <Snackbar ref={snackbarRef} text={error} />
+      <Form.Nav>
+        <Elements.Button
+          type='submit'
+          disabled={submitting ? true : false}
+          customCss={css`
+            width: 100%;
+          `}
+        >{(submitting) ? <Shared.Loader mode='light' /> : (appData && appData.content && appData.content.form.checkout.button_text)}</Elements.Button>
+      </Form.Nav>
+    </Form.Main>
+  ), [
+    formRef,
+    payment,
+    submitted,
+    submitting,
+    attemps,
+    snackbarRef,
+    showFieldErrors,
+    error,
+    appData,
+    dispatchFormErrors,
+    onSubmitHandler,
+    onChangeHandler,
+    onUpdateFieldHandler,
+  ]);
+});
+
+Component.displayName = 'CheckoutForm';
+export default Component;
---
+++ src/components/v1/Header/index.tsx
@@ -0,0 +1,92 @@
+import React, { FunctionComponent, useContext, useMemo } from 'react';
+import Elements from '../../Shared/Elements';
+import { pixelToRem, CustomCSSType } from 'meema.utils';
+import { css } from 'styled-components';
+import { Logo } from '../../Shared';
+import { AppContext } from '../../App/context';
+
+const MainHeader: FunctionComponent<{
+  customCss?: CustomCSSType;
+}> = ({
+  customCss,
+}) => {
+  const { appData } = useContext(AppContext);
+
+  return useMemo(() => (
+    <Elements.Header
+      customCss={css`
+        display: flex;
+        flex-direction: column;
+        justify-content: space-between;
+        padding: ${pixelToRem(40)};
+        width: 100%;
+        min-height: ${({theme}) => pixelToRem(theme.header.mobile.height)};
+        background-color: ${({theme}) => theme.header.mobile.backgroundColor};
+        background-position: center;
+        background-size: cover;
+        background-repeat: no-repeat;
+        transition: all 250ms ease;
+
+        ${appData && css`
+          background-image: linear-gradient(
+            0deg,
+            rgba(0, 0, 0, .75) 0%,
+            rgba(0, 0, 0, 0) 100%),
+            url(${process.env.REACT_APP_GREENLAB_API_IMAGES}/${appData && appData.content && appData.content.header.picture});
+        `}
+  
+        @media (min-width: ${({theme}) => pixelToRem(theme.responsive.tablet.minWidth)}) {
+          min-height: ${({theme}) => pixelToRem(theme.header.tablet.height)};
+          background-color: ${({theme}) => theme.header.tablet.backgroundColor};
+        }
+        
+        @media (min-width: ${({theme}) => pixelToRem(theme.responsive.desktop.minWidth)}) {
+          min-height: ${({theme}) => pixelToRem(theme.header.desktop.height)};
+          background-color: ${({theme}) => theme.header.desktop.backgroundColor};
+        }
+  
+        ${customCss && customCss};
+      `}
+    >
+      <Elements.Wrapper>
+        <Logo />
+      </Elements.Wrapper>
+      <Elements.Wrapper
+        customCss={css`
+          display: flex;
+          align-items: flex-end;
+          justify-content: space-between;
+          width: 100%;
+        `}
+      >
+        <Elements.HGroup
+          customCss={css`
+            @media (min-width: ${({theme}) => pixelToRem(theme.responsive.desktop.minWidth)}) {
+              padding-right: ${pixelToRem(10)};
+            }
+          `}
+        >
+          {appData && (
+            <Elements.H1
+              customCss={css`
+                color: white;
+                font-size: ${pixelToRem(24)};
+                font-family: ${({theme}) => theme.font.family.primary.bold};
+                letter-spacing: ${pixelToRem(0)};
+                
+                @media (min-width: ${({theme}) => pixelToRem(theme.responsive.desktop.minWidth)}) {
+                  font-size: ${pixelToRem(30)};
+                }
+              `}
+            >{appData && appData.content && appData.content.header.title}</Elements.H1>
+          )} 
+        </Elements.HGroup>
+      </Elements.Wrapper>
+    </Elements.Header>
+  ), [
+    customCss,
+    appData,
+  ])
+};
+
+export default MainHeader;

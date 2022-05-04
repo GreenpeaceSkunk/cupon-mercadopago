384d0fef1528a6f6efe336dcc9facbd86ecce0f3
Dan Tovbein <dtovbein@gmail.com>
2022-05-04 14:10:50 -0300

    Update App component

--- src/components/App/context.tsx
+++ src/components/App/context.tsx
@@ -1,8 +1,7 @@
-import React, { createContext, useEffect, useMemo, useReducer, useState } from "react";
+import React, { createContext, lazy, Suspense, useEffect, useMemo, useReducer, useState } from "react";
 import useQuery from "../../hooks/useQuery";
 import { ThemeProvider } from 'styled-components';
 import { GlobalStyle } from '../../theme/globalStyle';
-import Theme from '../../theme/Theme';
 import ErrorBoundary from '../ErrorBoundary';
 import { getCoupon } from "../../services/greenlab";
 import { initialState, reducer } from './reducer';
@@ -12,6 +11,13 @@ import { initialize as initializeMercadopago } from '../../utils/mercadopago';
 import { initialize as inititalizeAnalytics } from '../../utils/googleAnalytics';
 import { initialize as initializeHotjar } from '../../utils/hotjar';
 import { initialize as initializeHubspot } from '../../utils/hubspot';
+
+// TODO: Investigate how to load dinamically
+import ThemeV1 from '../../theme/v1/Theme';
+import ThemeV2 from '../../theme/v2/Theme';
+// import Theme from `../../theme/v${window.localStorage.greenlab_app_design_version}/Theme`;
+
+// const Theme = lazy(() => import(`../../theme/v${window.localStorage.greenlab_app_design_version}/Theme`));
 
 interface IContext {
   urlSearchParams: URLSearchParams;
@@ -39,7 +45,8 @@ const ContextProvider: React.FunctionComponent<IProps> = ({ children }) => {
       (async () => {
         const payload = await getCoupon(appName) as any;
         if(payload) {
-          window.sessionStorage.setItem('greenlab_app', payload.name)
+          window.sessionStorage.setItem('greenlab_app_name', payload.name);
+          window.sessionStorage.setItem('greenlab_app_design_version', payload.features.use_design_version ? payload.features.use_design_version : 1);
           dispatch({
             type: 'SET_APP_DATA',
             payload,
@@ -89,7 +96,7 @@ const ContextProvider: React.FunctionComponent<IProps> = ({ children }) => {
       isOpen,
       setIsOpen,
     }}>
-      <ThemeProvider theme={Theme}>
+      <ThemeProvider theme={(appData && appData.features && appData.features.use_design_version === 2) ? ThemeV2 : ThemeV1}>
         <GlobalStyle />
         <ErrorBoundary>
           { children }

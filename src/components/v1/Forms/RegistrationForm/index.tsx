ecdaec95d1ec268509cefcc0972a6ddda37408bb
Dan Tovbein <dtovbein@gmail.com>
2022-05-04 14:09:30 -0300

    Create v1 of components

---
+++ src/components/v1/Forms/RegistrationForm/index.tsx
@@ -0,0 +1,368 @@
+import React, { FormEvent, memo, useCallback, useContext, useEffect, useMemo, useReducer, useRef, useState } from 'react';
+import { generatePath, useNavigate } from 'react-router-dom';
+import { FormContext } from '../../../Forms/context';
+import { OnChangeEvent } from 'greenpeace';
+import {
+  validateEmail,
+  validateNewAmount,
+  validatePhoneNumber,
+  validateAreaCode,
+  validateEmptyField,
+  validateFirstName,
+} from '../../../../utils/validators';
+import { css } from 'styled-components';
+import Shared from '../../../Shared';
+import Elements from '../../../Shared/Elements';
+import { addOrRemoveSlashToDate } from '../../../../utils';
+import { initialState, reducer } from '../../../Forms/RegistrationForm/reducer';
+import { pushToDataLayer } from '../../../../utils/googleTagManager';
+// import { pixelToRem } from 'meema.utils';
+import useQuery from '../../../../hooks/useQuery';
+import Snackbar, { IRef as ISnackbarRef } from '../../../Snackbar';
+import { createContact } from '../../../../services/greenlab';
+import { AppContext } from '../../../App/context';
+
+const Component: React.FunctionComponent<{}> = memo(() => {
+  const { appData } = useContext(AppContext);
+  const {
+    data: {
+      user: {
+        firstName,
+        lastName,
+        birthDate,
+        email,
+        areaCode,
+        phoneNumber,
+      },
+      payment: {
+        amount,
+        newAmount,
+      },
+    },
+    params,
+    dispatch,
+  } = useContext(FormContext);
+  const [{ submitting, allowNext }, dispatchFormErrors ] = useReducer(reducer, initialState);
+  const [ showFieldErrors, setShowFieldErrors ] = useState<boolean>(false);
+  const navigate = useNavigate();
+  const { searchParams } = useQuery();
+  const snackbarRef = useRef<ISnackbarRef>(null);
+  
+  const onChangeHandler = useCallback((evt: OnChangeEvent) => {
+    evt.preventDefault();
+    const name = evt.currentTarget.name;
+    let value = evt.currentTarget.value;
+
+    if(name === 'birthDate') {
+      value = addOrRemoveSlashToDate((value.length < birthDate.length && birthDate.charAt(birthDate.length - 1) === '/') ? birthDate : value);
+    }
+
+    if(name === 'amount' || name === 'newAmount') {
+      dispatch({
+        type: 'UPDATE_PAYMENT_DATA',
+        payload: { [name]: value }
+      });
+    } else {
+      dispatch({
+        type: 'UPDATE_USER_DATA',
+        payload: { [name]: value }
+      });
+    }
+  }, [
+    birthDate,
+    dispatch,
+  ]);
+
+  const onUpdateFieldHandler = useCallback((fieldName: string, isValid: boolean, value: any) => {
+    if(fieldName === 'amount' || fieldName === 'newAmount') {
+      dispatchFormErrors({
+        type: 'UPDATE_FIELD_ERRORS',
+        payload: {
+          fieldName: 'newAmount',
+          isValid: 
+          (fieldName === 'amount' && value === 'otherAmount')
+          ? false
+          : (fieldName === 'newAmount' && amount === 'otherAmount')
+          ? validateNewAmount(value).isValid
+          : true,
+        }
+      });
+    } else {
+      dispatchFormErrors({
+        type: 'UPDATE_FIELD_ERRORS',
+        payload: {
+          fieldName,
+          isValid,
+        }
+      });
+    }
+  }, [
+    amount,
+    dispatchFormErrors,
+  ]);
+
+  const onSubmitHandler = useCallback(async (evt: FormEvent) => {
+    evt.preventDefault();
+    
+    if(!allowNext) {
+      setShowFieldErrors(true);
+      if(snackbarRef && snackbarRef.current) {
+        snackbarRef.current.showSnackbar();
+      }
+    } else {
+      dispatchFormErrors({
+        type: 'SUBMIT',
+      });
+
+      const contact = await createContact({
+        email,
+        firstname: firstName,
+        lastname: lastName,
+        phone: `${areaCode}${phoneNumber}`,
+        donationStatus: 'requested',
+      });
+
+      if(contact) {
+        pushToDataLayer({ 'event' : 'petitionSignup' });
+
+        if(params) {
+          navigate({
+            pathname: generatePath('/:couponType/forms/:formType', {
+              couponType: params.couponType,
+              formType: 'checkout',
+            }),
+            search: searchParams,
+          }, { replace: true });
+        }
+      }
+    }
+  }, [
+    firstName,
+    lastName,
+    email,
+    areaCode,
+    phoneNumber,
+    params,
+    allowNext,
+    navigate,
+    searchParams,
+  ]);
+
+  useEffect(() => {
+    if(appData && appData.content) {
+      if(appData.content.amounts.values.filter((v: number) => v === appData.content.amounts.default).length) {
+        dispatch({
+          type: 'UPDATE_PAYMENT_DATA',
+          payload: { 'amount': `${appData.content.amounts.default}` }
+        });
+      } else {
+        dispatch({
+          type: 'UPDATE_PAYMENT_DATA',
+          payload: { 
+            'amount': 'otherAmount',
+            'newAmount': `${appData.content.amounts.default}`,
+          }
+        });
+      }
+    }
+  }, [
+    appData,
+    dispatch,
+  ]);
+  
+  return useMemo(() => (
+    <Shared.Form.Main id='sign-form' onSubmit={onSubmitHandler}>
+      <Shared.Form.Header>
+        <Elements.HGroup>
+          <Shared.Form.Title>{appData && appData.content && appData.content.form.registration.title}</Shared.Form.Title>
+        </Elements.HGroup>
+        <Shared.General.Text>{appData && appData.content && appData.content.form.registration.text}</Shared.General.Text>
+      </Shared.Form.Header>
+      <Shared.Form.Content>
+        <Shared.Form.Row>
+          <Shared.Form.Column>
+            <Shared.Form.Group
+              value={amount}
+              fieldName='amount'
+              labelText={`${params.couponType === 'oneoff' ? 'Autorizo el pago por única vez de:' : 'Autorizo el débito automático mensual de:'}`}
+              showErrorMessage={showFieldErrors}
+              validateFn={validateEmptyField}
+              onUpdateHandler={onUpdateFieldHandler}
+              >
+                {appData && appData.content && appData.content.amounts.values.map((value: number) => (
+                  <Shared.Form.RadioButton
+                    key={`${value}`}
+                    text={`$${value}`}
+                    name='amount'
+                    value={`${value}`}
+                    checkedValue={amount}
+                    onChangeHandler={onChangeHandler}
+                  />
+                ))}
+                <Shared.Form.RadioButton
+                  key='otherAmount'
+                  text='Otras donaciones'
+                  name='amount'
+                  value='otherAmount'
+                  checkedValue={amount}
+                  onChangeHandler={onChangeHandler}
+                />
+            </Shared.Form.Group>
+            </Shared.Form.Column>
+            {(amount === 'otherAmount') ? (
+              <Shared.Form.Column>
+                <Shared.Form.Group
+                  fieldName='newAmount'
+                  value={newAmount}
+                  labelText='Ingrese el monto'
+                  showErrorMessage={showFieldErrors}
+                  validateFn={validateNewAmount}
+                  onUpdateHandler={onUpdateFieldHandler}
+                >
+                  <Elements.Input
+                    name='newAmount'
+                    type='text'
+                    disabled={!(amount === 'otherAmount')} 
+                    value={newAmount}
+                    placeholder='Ej. $350'
+                    maxLength={8}
+                    onChange={onChangeHandler}
+                  />
+                </Shared.Form.Group>
+              </Shared.Form.Column>
+            ) : null}
+        </Shared.Form.Row>
+        <Shared.Form.Row>
+          <Shared.Form.Column>
+            <Shared.Form.Group
+              value={email}
+              fieldName='email'
+              labelText='Correo electrónico'
+              showErrorMessage={showFieldErrors}
+              validateFn={validateEmail}
+              onUpdateHandler={onUpdateFieldHandler}
+            >
+              <Elements.Input
+                name='email'
+                type='email'
+                placeholder='Ej. daniela.lopez@email.com'
+                value={email}
+                onChange={onChangeHandler}
+              />
+            </Shared.Form.Group>
+          </Shared.Form.Column>
+        </Shared.Form.Row>
+        <Shared.Form.Row>
+          <Shared.Form.Column>
+            <Shared.Form.Group
+              fieldName='firstName'
+              value={firstName}
+              labelText='Nombre'
+              showErrorMessage={showFieldErrors}
+              validateFn={validateFirstName}
+              onUpdateHandler={onUpdateFieldHandler}
+            >
+              <Elements.Input
+                name='firstName'
+                type='text'
+                placeholder='Ej. Lucas'
+                value={firstName}
+                onChange={onChangeHandler}
+              />
+            </Shared.Form.Group>
+            <Shared.Form.Group
+              fieldName='lastName'
+              value={lastName}
+              labelText='Apellido'
+              showErrorMessage={showFieldErrors}
+              validateFn={validateFirstName}
+              onUpdateHandler={onUpdateFieldHandler}
+            >
+              <Elements.Input
+                name='lastName'
+                type='text'
+                placeholder='Ej. Rodriguez'
+                value={lastName}
+                onChange={onChangeHandler}
+              />
+            </Shared.Form.Group>
+          </Shared.Form.Column>
+        </Shared.Form.Row>
+        <Shared.Form.Row>
+          <Shared.Form.Column
+            bottomText='Escribe solo números y no agregues guiones.'
+          >
+            <Shared.Form.Group
+              fieldName='areaCode'
+              value={areaCode}
+              labelText='Cód. área'
+              showErrorMessage={showFieldErrors}
+              validateFn={validateAreaCode}
+              onUpdateHandler={onUpdateFieldHandler}
+              customCss={css`
+                width: 40%;
+              `}
+            >
+              <Elements.Input
+                name='areaCode'
+                type='text'
+                placeholder='Ej. 11'
+                value={areaCode}
+                maxLength={4}
+                onChange={onChangeHandler}
+              />
+            </Shared.Form.Group>
+            <Shared.Form.Group
+              fieldName='phoneNumber'
+              value={phoneNumber}
+              labelText='Número telefónico'
+              showErrorMessage={showFieldErrors}
+              validateFn={validatePhoneNumber}
+              onUpdateHandler={onUpdateFieldHandler}
+            >
+              <Elements.Input
+                name='phoneNumber'
+                type='text'
+                placeholder='Ej. 41239876'
+                value={phoneNumber}
+                onChange={onChangeHandler}
+              />
+            </Shared.Form.Group>
+          </Shared.Form.Column>
+        </Shared.Form.Row>
+      </Shared.Form.Content>
+      <Snackbar
+        ref={snackbarRef}
+        text='Tenés campos incompletos o con errores. Revisalos para continuar.'
+      />
+      <Shared.Form.Nav>
+        <Elements.Button
+          type='submit'
+          disabled={(submitting) ? true : false}
+          customCss={css`
+            width: 100%;
+          `}
+        >{(submitting) ? <Shared.Loader mode='light' /> : (appData && appData.content && appData.content.form.registration.button_text)}</Elements.Button>
+      </Shared.Form.Nav>
+    </Shared.Form.Main>
+  ), [
+    firstName,
+    lastName,
+    phoneNumber,
+    amount,
+    newAmount,
+    email,
+    areaCode,
+    submitting,
+    params,
+    snackbarRef,
+    showFieldErrors,
+    appData,
+    onSubmitHandler,
+    onChangeHandler,
+    onUpdateFieldHandler,
+  ]);
+});
+
+Component.displayName = 'RegistrationForm';
+export default Component;
---
+++ src/components/v1/Home/index.tsx
@@ -0,0 +1,161 @@
+import React, { useMemo, lazy, Suspense, memo, useRef, useContext, MouseEvent } from 'react';
+import Elements from '../../Shared/Elements';
+import Shared from '../../Shared';
+import styled, { css } from 'styled-components';
+import { pixelToRem } from 'meema.utils';
+import ErrorBoundary from '../../ErrorBoundary';
+import { AppContext } from '../../App/context';
+import ModalOpenForm from '../ModalOpenForm';
+import { Outlet } from 'react-router';
+
+const Header = lazy(() => import('../Header'));
+const Footer = lazy(() => import('../../Footer'));
+
+const Component: React.FunctionComponent<{}> = memo(() => {
+  const viewRef = useRef<HTMLElement>(null);
+  const { appData, setIsOpen } = useContext(AppContext);
+
+  return useMemo(() => (
+    <>
+      <Elements.View
+        ref={viewRef}
+        customCss={css`
+          display: flex;
+          flex-direction: column;
+          width: 100%;
+          min-height: 100vh;
+          background-size: cover;
+          background-repeat: no-repeat;
+          background-position: center;
+          
+          @media (min-width: ${({theme}) => pixelToRem(theme.responsive.desktop.minWidth)}) {
+            flex-direction: row;
+          }
+        `}
+      >
+        <Elements.Wrapper
+          customCss={css`
+            position: relative;
+            display: flex;
+            flex-direction: column;
+            width: 100%;
+            min-height: 100vh;
+            transition: all 250ms ease;
+
+            @media (min-width: ${({theme}) => pixelToRem(theme.responsive.desktop.minWidth)}) {
+              min-height: 100vh;
+              padding-bottom: 0;
+              display: flex;
+            }
+          `}
+        >
+          <ErrorBoundary fallback='Header Error.'>
+            <Suspense fallback={<Shared.Loader />}>
+              <Header />
+            </Suspense>
+          </ErrorBoundary>
+
+          <Elements.Wrapper
+            customCss={css`
+              display: flex;
+              flex-direction: column;
+              width: 100%;
+              padding: 0;
+            `}
+          >
+            <Elements.Wrapper
+              customCss={css`
+                padding: ${pixelToRem(30)} ${pixelToRem(40)};
+
+                > * {
+                  margin-bottom: ${pixelToRem(28)};
+
+                  &:last-child {
+                    margin-bottom: 0;
+                  }
+                }
+
+                @media (min-width: ${({theme}) => pixelToRem(theme.responsive.desktop.minWidth)}) {
+                  padding-right: ${pixelToRem(70)};
+                }
+              `}
+            >
+              <Elements.H1
+                customCss={css`
+                  color: ${({theme}) => theme.color.secondary.dark};
+                  font-size: ${pixelToRem(20)};
+                  font-family: ${({theme}) => theme.font.family.primary.bold};
+                  line-height: 1.2;
+                  
+                  @media (min-width: ${({theme}) => pixelToRem(theme.responsive.desktop.minWidth)}) {
+                    font-size: ${pixelToRem(24)};
+                  }
+                `}
+              >{appData && appData.content && appData.content.home.title}</Elements.H1>
+              <Elements.WrapperHtml
+                customCss={css`
+                  color: ${({theme}) => theme.color.secondary.dark};
+                  font-size: ${pixelToRem(18)};
+                  line-height: 140%;
+                `}
+                dangerouslySetInnerHTML={{__html: appData && appData.content && appData.content.home.text }}
+              />
+              <Elements.Span
+                customCss={css`
+                  font-family: ${({theme}) => theme.font.family.primary.bold};
+                  font-size: ${pixelToRem(18)};
+                  color: ${({theme}) => theme.color.primary.normal};
+
+                  @media (min-width: ${({theme}) => pixelToRem(theme.responsive.desktop.minWidth)}) {
+                    font-size: ${pixelToRem(24)};
+                  }
+                `}
+              >{appData && appData.content && appData.content.home.highlighted_text}</Elements.Span>
+              <Elements.Button
+                onClick={(evt: MouseEvent<HTMLButtonElement>) => {setIsOpen(true)}}
+                customCss={css`
+                  width: 100%;
+                  box-shadow: 0 ${pixelToRem(4)} ${pixelToRem(14)} rgba(0, 0, 0, .25);
+                  margin: ${pixelToRem(20)} 0;
+
+                  @media (min-width: ${({theme}) => pixelToRem(theme.responsive.desktop.minWidth)}) {
+                    display: none;
+                  }
+                `}
+              >{appData && appData.content && appData.content.home.button_text}</Elements.Button>
+            </Elements.Wrapper>
+          </Elements.Wrapper>
+        </Elements.Wrapper>
+        <Elements.Wrapper
+          customCss={css`
+            position: relative;
+            flex-shrink: 0;
+            flex-grow: 0;
+            width: 100%;
+
+            @media (min-width: ${({theme}) => pixelToRem(theme.responsive.desktop.minWidth)}) {
+              width: ${pixelToRem(480)};
+            }
+          `}
+        >
+          <Outlet />
+        </Elements.Wrapper>
+      </Elements.View>
+      <ErrorBoundary fallback='Footer Error.'>
+        <Suspense fallback={<Shared.Loader />}>
+          <Elements.Wrapper>  
+            <Footer />
+          </Elements.Wrapper>
+        </Suspense>
+      </ErrorBoundary>
+      <ModalOpenForm />
+    </>
+  ), [
+    viewRef,
+    appData,
+    setIsOpen,
+  ]);
+});
+
+Component.displayName = 'Home';
+export default Component;

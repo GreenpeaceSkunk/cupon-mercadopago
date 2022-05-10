007084fc0f204d6f4e791c7edfaf8f5495719f49
Dan Tovbein <dtovbein@gmail.com>
2022-05-10 17:13:39 -0300

    Generic: Add origin field to sync to HS

--- src/services/greenlab.ts
+++ src/services/greenlab.ts
@@ -55,7 +55,10 @@ export const postRecord = async (data: any) => {
       },
       baseURL: `${process.env.REACT_APP_GREENLAB_API_URL}/forma/form/${data.form_id}/record`,
       method: 'POST',
-      data,
+      data: {
+        ...data,
+        origin: document.location.href,
+      },
     });
     return response;
   } catch(error: any) {

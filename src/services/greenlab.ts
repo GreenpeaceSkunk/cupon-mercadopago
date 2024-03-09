821ecc3d5f09b03ac6c396063c96512660d17a4c
Dan Tovbein <dtovbein@gmail.com>
2024-03-09 08:22:35 -0300

    Add mising functions

--- src/services/greenlab.ts
+++ src/services/greenlab.ts
@@ -94,3 +94,75 @@ export const postRecord = async (data: any, formId?: number) => {
     console.log(error);
   }
 }
+
+export const saveLocal = (formId: number, couponName: string, country: string, data: any) => {
+  const dbLocalName = `__coupon_${couponName}_${country}`.toLowerCase();
+  let dbLocal = window.localStorage.getItem(dbLocalName) || [];
+
+  if(typeof dbLocal === 'string') {
+    dbLocal = JSON.parse(dbLocal);
+  }
+
+  const txnDate = new Date(data.txnDate);
+
+  (dbLocal as any).push({
+    formId,
+    id: txnDate.getTime(),
+    date: `${txnDate.getDate()}/${txnDate.getMonth()+1}/${txnDate.getFullYear()} ${txnDate.getHours()}:${txnDate.getMinutes()}:${txnDate.getSeconds()}`,
+    synced: false,
+    data,
+  });
+
+  window.localStorage.setItem(dbLocalName, JSON.stringify(dbLocal));
+}
+
+/**
+ *
+ * @param couponName
+ * @param country
+ * @returns
+ */
+export const getLocal = (couponName: string, country: string): Array<any> => {
+  const dbLocalName = `__coupon_${couponName}_${country}`.toLowerCase();
+  let dbLocal = window.localStorage.getItem(dbLocalName) || [];
+
+  if(typeof dbLocal === 'string') {
+    return JSON.parse(dbLocal);
+  }
+
+  return dbLocal as Array<any>;
+}
+
+/**
+ * This function syncs all non-synced records from local.
+ * @param formId ForMa ID
+ * @param couponName Coupon Name
+ * @param country Country
+ * @returns
+ */
+export const syncLocal = async (couponName: string, country: string):Promise<any> => {
+  let dbLocal = getLocal(couponName, country);
+  const filteredDbLocal = dbLocal.map(data => !data.synced ? data : null);
+
+  return await Promise.all(
+    filteredDbLocal.map(data => data ? postRecord(data.data, data.formId) : "")
+  ).then(response => {
+    response.map((item: any, idx: number) => {
+      let filteredItem = filteredDbLocal[idx];
+
+      if(item.error === true) {
+        console.log(`${filteredItem.id} cannot be synced`);
+      } else {
+        if(filteredItem) {
+          console.log(`${filteredItem.id} synced correctly.`);
+          filteredItem.synced = true;
+          dbLocal = dbLocal.map((item: any) => item.id === filteredItem.id ? filteredItem : item)
+        }
+      }
+
+      const dbLocalName = `__coupon_${couponName}_${country}`.toLowerCase();
+      window.localStorage.setItem(dbLocalName, JSON.stringify(dbLocal));
+    });
+    return dbLocal;
+  });
+}
